6ff8ae17431b275c7d77b327a06a6b4fbb3c688f
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2021-12-14 10:36:48 -0300

    feat(layouts): add navbar layout

--- resources/views/layouts/bootstrap5.blade.php
+++ resources/views/layouts/bootstrap5.blade.php
@@ -23,14 +23,16 @@
 </head>
 
 <body class="text-center aligns-items-center justify-content-center">
+
+    <!-- Content body -->
     @yield('body')
 
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    @yield('customJS')
     <!-- MDB -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.1/mdb.min.js"></script>
-
+    <!-- Custom JS -->
+    @yield('customJS')
 </body>
 
 </html>
