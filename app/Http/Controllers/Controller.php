2a66c7a6c4ffe78cd6429c5b562800b57af1f176
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2021-12-22 12:35:07 -0300

    fix: wrong register

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,36 +7,11 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\View;
+use Illuminate\Http\Request;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-
-    protected  $viewModel = [
-        'nombre' => null,
-        'apellido' => null,
-    ];
-
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
-
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        View::share('nombre', 'Tomas');
-    }
+    protected  $viewModel = [];
 }
