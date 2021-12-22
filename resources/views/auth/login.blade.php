e44427abd2db1ff3cbece3d4bd82a134b9a8d6bc
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2021-12-22 14:22:06 -0300

    style(login): resonsiveness & color

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,20 +18,20 @@
             <form action="{{ route('clientes.inicio') }}" method="post">
                 @csrf
                 <div class="form-outline my-3">
-                    <input type="email" class="form-control form-control-lg" id="floatingInput" placeholder="tu@mail.com" name="usuario" />
+                    <input type="email" class="form-control form-control-lg text-light" id="floatingInput" placeholder="tu@mail.com" name="usuario" />
                     @error('email')
                     <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
                     </span>
                     @enderror
-                    <label for="floatingInput" class="form-label">Mail</label>
+                    <label for="floatingInput" class="form-label text-light">Mail</label>
                 </div>
                 <div class="form-outline my-1">
-                    <input type="password" class="form-control form-control-lg {{ Session::get('invalidPw') ?? '' }}" id="floatingPassword" placeholder="Contraseña" name="contraseña" />
+                    <input type="password" class="form-control form-control-lg {{ Session::get('invalidPw') ?? '' }} text-light" id="floatingPassword" placeholder="Contraseña" name="contraseña" />
                     <span class="invalid-feedback" role="alert">
                         <strong>Contraseña incorrecta</strong>
                     </span>
-                    <label for="floatingPassword" class="form-label">Contraseña</label>
+                    <label for="floatingPassword" class="form-label text-light">Contraseña</label>
                 </div>
                 <div class="d-grid">
                     <button type="submit" class="btn btn-secondary mb-2" id="iniciar">
