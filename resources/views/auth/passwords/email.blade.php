1729e56c0a487157867fd6fe7b3e19c809d747ad
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2021-12-23 13:27:07 -0300

    style(view): updated to match

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,46 +1,44 @@
-@extends('layouts.app')
+@extends('layouts.bootstrap5')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+@section('title', 'Resetear Contraseña')
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+@section('customCSS')
+<link rel="stylesheet" href="{{asset('css/style.css')}}" />
+@endsection
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+@section('body')
+<div class="flex-container">
+    <div class="table">
+        <div class="container text-center border-2 mt-2">
+            <img src="{{asset('assets/brand.png')}}" height="300" width="120" class="img-fluid mt-2" />
+            <h2 class="text-light">Olvidé mi Contraseña</h2>
+        </div>
+        <div class="container">
+            <!--div class="abs-center"-->
+            <form action="{{ route('password.email') }}" method="post">
+                @csrf
+                <!--Email-->
+                <div class="form-outline my-3 form-white">
+                    <input id="email" type="email" class="form-control form-control-lg text-light" name="email" value="{{ $email ?? old('email') }}" placeholder="tu@mail.com" required autocomplete="email" autofocus />
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                    <label for="email" class="form-label text-light">Mail</label>
                 </div>
-            </div>
+                <div class="d-grid">
+                    <button type="submit" class="btn btn-secondary btn-rounded mb-2" id="updatePassword">
+                        Reestablecer
+                    </button>
+                    @if (Route::has('login'))
+                    <a class="mb-2 text-light" href="{{ route('login') }}">{{ __('Iniciar Sesión') }}</a>
+                    @endif
+                    @if (Route::has('register'))
+                    <a class="mb-2 text-light" href="{{ route('register') }}">{{ __('No poseo cuenta, Registrarme') }}</a>
+                    @endif
+                </div>
+            </form>
         </div>
     </div>
 </div>
