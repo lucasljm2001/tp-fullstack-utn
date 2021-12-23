57d7f14707d26e9931fba2ae206432af57e3ad68
Franco Parente <fparente14@frba.utn.edu.ar>
2021-12-23 19:29:41 -0300

    avances landing

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -2,18 +2,32 @@
 
 @section('title', 'Inicio')
 
+@section('customJS')
+<script type="text/javascript" src="{{asset('js/landing.js')}}"></script>
+@endsection
+
+@section('customCSS')
+<link rel="stylesheet" href="{{asset('css/landing.css')}}">
+@endsection
+
 @section('body')
 <!-- Navbar -->
 @parent
 <!-- Content -->
 
-<main>
-    <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, dolorem.
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, laborum?
-    </p>    
-</main>
+<div id="section1">
+    <div class="container">
+        <div class="row justify-content-end align-items-center">
+            <div class="col-8 text-white">
+                <h1>My Gym</h1>
+                <h2>Queremos hacerte lindo y fuerte papito</h2>
+            </div>
+            <div class="col-4">
+                <iframe src='https://my.spline.design/cylindersanimationcopy-43c0e5e1ed68a1131b25d698596a2193/' frameborder='0' width='100%' height='700px'></iframe>
+            </div>
+        </div>
+    </div>
+</div>
 <section>
     <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, dolorem.
@@ -42,8 +56,4 @@
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, laborum?
     </p>    
 </footer>
-@endsection
-
-@section('customJS')
-<script type="text/javascript" src="{{asset('js/landing.js')}}"></script>
 @endsection
