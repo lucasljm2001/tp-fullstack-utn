48386b96ea8092dfdba26e2341d35272f68f036f
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2021-12-26 11:49:56 -0300

    fix(navbar): responsiveness

--- resources/views/layouts/with-navbar.blade.php
+++ resources/views/layouts/with-navbar.blade.php
@@ -13,91 +13,85 @@
         <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <i class="fas fa-bars"></i>
         </button>
-        <a class="navbar-brand mt-2 mt-lg-0" href="#">
-            <img src="{{asset('assets/brand.png')}}" height="45" width="45" alt="App logo" loading="lazy" />
-        </a>
-        <h3 style="font-family: 'Bebas Neue', cursive">My Gym</h3>
+
         <!-- Collapsible wrapper -->
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Navbar brand -->
-
-
-            <!-- Left links -->
-
+            <a class="navbar-brand mt-2 mt-lg-0" href="#">
+                <img src="{{asset('assets/brand.png')}}" height="45" width="45" alt="App logo" loading="lazy" />
+                <h3 style="font-family: 'Bebas Neue', cursive">My Gym</h3>
+            </a>
+            <!-- Left Content -->
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 text-start">
+                    <li class="nav-item">
+                        <a class="nav-link {{ Request::is('/') ||  Request::is('home') ? 'active' : ''}}" href="{{route('user.home')}}" style="font-family: 'Montserrat', sans-serif">
+                            Home
+                        </a>
+                    </li>
+                    <li class="nav-item {{ Request::is('team') ? 'active' : ''}}">
+                        <a class="nav-link" href="#" style="font-family: 'Montserrat', sans-serif">
+                            Team
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link {{ Request::is('projects') ? 'active' : ''}}" href="#" style="font-family: 'Montserrat', sans-serif">
+                            Projects
+                        </a>
+                    </li>
+                    @auth
+                    @else
+                    <li class="nav-item">
+                        <a class="btn btn-outline-warning" href="{{ route('login') }}" style="font-family: 'Montserrat', sans-serif">
+                            Log In
+                        </a>
+                    </li>
+                    @endauth
+                </ul>
+            </div>
+            <!-- Left Content -->
         </div>
-
         <!-- Collapsible wrapper -->
+
         <!-- Right elements -->
         <div class="d-flex align-items-center justify-content-end">
+            <!-- Right links -->
             <!-- Icon
             <a class="text-reset me-3" href="#">
                 <i class="fas fa-shopping-cart"></i>
             </a> -->
-
+            @auth
+            <!-- Notifications -->
+            <a class="text-reset me-3 dropdown-toggle hidden-arrow" href="#" id="navbarDropdownMenuLink" role="button" data-mdb-toggle="dropdown" aria-expanded="false">
+                <i class="fas fa-calendar-week fs-lg"></i>
+                <span class="badge rounded-pill badge-notification bg-danger">{{ Session::get('dias')?? '0'}}</span>
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
+                <li>
+                    <a class="dropdown-item" href="{{ route('turnos.mostrar', ['id'=>Session::get('id')])}}">Mis turnos</a>
+                </li>
+            </ul>
+            <!-- Avatar -->
+            <a class="dropdown-toggle d-flex align-items-center hidden-arrow" href="#" id="navbarDropdownMenuLink" role="button" data-mdb-toggle="dropdown" aria-expanded="false">
+                <img src="{{Session::get('profile') ?? 'https://upload.wikimedia.org/wikipedia/commons/f/f4/User_Avatar_2.png'}}" width="45" height="45" class="rounded-circle" loading="lazy" alt="Black and White Portrait of a Man" />
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
+                <li>
+                    <a class="dropdown-item disabled" href="{{ route('clientes.inicio') }}">Mi perfil</a>
+                </li>
+                <li>
+                    <a class="dropdown-item disabled" href="#">Ajustes</a>
+                </li>
+                <li>
+                    <a class="dropdown-item" href="{{ route('clientes.index') }}">Cerrar sesion</a>
+                </li>
+            </ul>
+            @else
+            @endauth
         </div>
         <!-- Right elements -->
     </div>
     <!-- Container wrapper -->
-    <div class="container-fluid justify-content-end">
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Right links -->
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0 text-start">
-                <li class="nav-item">
-                    <a class="nav-link {{ Request::is('/') ||  Request::is('home') ? 'active' : ''}}" href="{{route('user.home')}}" style="font-family: 'Montserrat', sans-serif">
-                        Home
-                    </a>
-                </li>
-                <li class="nav-item {{ Request::is('team') ? 'active' : ''}}">
-                    <a class="nav-link" href="#" style="font-family: 'Montserrat', sans-serif">
-                        Team
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ Request::is('projects') ? 'active' : ''}}" href="#" style="font-family: 'Montserrat', sans-serif">
-                        Projects
-                    </a>
-                </li>
-                @auth
-                @else
-                <li class="nav-item">
-                    <a class="btn btn-outline-warning" href="{{ route('login') }}" style="font-family: 'Montserrat', sans-serif">
-                        Log In
-                    </a>
-                </li>
-                @endauth
-            </ul>
-        </div>
-        @auth
-        <!-- Notifications -->
-        <a class="text-reset me-3 dropdown-toggle hidden-arrow" href="#" id="navbarDropdownMenuLink" role="button" data-mdb-toggle="dropdown" aria-expanded="false">
-            <i class="fas fa-calendar-week fs-lg"></i>
-            <span class="badge rounded-pill badge-notification bg-danger">{{ Session::get('dias')?? '0'}}</span>
-        </a>
-        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
-            <li>
-                <a class="dropdown-item" href="{{ route('turnos.mostrar', ['id'=>Session::get('id')])}}">Mis turnos</a>
-            </li>
-        </ul>
-
-        <!-- Avatar -->
-        <a class="dropdown-toggle d-flex align-items-center hidden-arrow" href="#" id="navbarDropdownMenuLink" role="button" data-mdb-toggle="dropdown" aria-expanded="false">
-            <img src="{{Session::get('profile') ?? 'https://upload.wikimedia.org/wikipedia/commons/f/f4/User_Avatar_2.png'}}" width="45" height="45" class="rounded-circle" loading="lazy" alt="Black and White Portrait of a Man" />
-        </a>
-        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
-            <li>
-                <a class="dropdown-item disabled" href="{{ route('clientes.inicio') }}">Mi perfil</a>
-            </li>
-            <li>
-                <a class="dropdown-item disabled" href="#">Ajustes</a>
-            </li>
-            <li>
-                <a class="dropdown-item" href="{{ route('clientes.index') }}">Cerrar sesion</a>
-            </li>
-        </ul>
-        @else
-        @endauth
-    </div>
-
 </nav>
 <!-- Navbar -->
 @endsection
