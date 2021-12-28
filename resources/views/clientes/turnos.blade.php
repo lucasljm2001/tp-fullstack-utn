0b4c42614bf9a6814d15f30b8af855a8e8fe39f8
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2021-12-28 11:19:18 -0300

    fix: remove already imported jQuery

--- resources/views/clientes/turnos.blade.php
+++ resources/views/clientes/turnos.blade.php
@@ -9,7 +9,7 @@
 
 
 @section('customJS')
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+@parent
 <script src="{{asset('js/turnos.js')}}"></script>
 @endsection
 
