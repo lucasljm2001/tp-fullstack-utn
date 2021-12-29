11339fef65c944ea494a92c5d0b0e358e7ea2d92
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2021-12-29 10:49:17 -0300

    style: add app-name

--- resources/views/layouts/bootstrap5.blade.php
+++ resources/views/layouts/bootstrap5.blade.php
@@ -3,7 +3,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <title>appname - @yield('title')</title>
+    <title>Fitbox - @yield('title')</title>
     <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
